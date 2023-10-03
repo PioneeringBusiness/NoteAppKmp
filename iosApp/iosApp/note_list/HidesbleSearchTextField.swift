7afe6883d3902195a25cde31c7a51f33d3b656ca
Charles Yost <64622858+PioneeringBusiness@users.noreply.github.com>
2023-10-02 20:28:04 -0600

    Start Note List Screen in iOS

---
+++ iosApp/iosApp/note_list/HidesbleSearchTextField.swift
@@ -0,0 +1,22 @@
+//
+//  HidesbleSearchTextField.swift
+//  iosApp
+//
+//  Created by Charles Yost on 10/2/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+// TODO start at https://www.youtube.com/watch?v=1w-LTUm_iDE 2:12:28
+struct HidesbleSearchTextField: View {
+    var body: some View {
+        Text("Hello, Charlie! You are aswesome")
+    }
+}
+
+struct HidesbleSearchTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        HidesbleSearchTextField()
+    }
+}
